085600c102e370551fec0d2ad7a8cf2572e185f7
Mok <mokhtar@themok.company>
2025-12-21 12:40:41 +0000

    first fresh commit

---
+++ config/system-addons.php
@@ -0,0 +1,56 @@
+<?php return array (
+  'admin_panel' => 
+  array (
+    'active' => '1',
+    'username' => 'mokmo',
+    'purchase_key' => '0e33a520-41c7-41f0-80d8-f69a21e21aac',
+    'software_id' => 'MzY3NzIxMTI=',
+    'domain' => 'phpstack-1485306-6088382.cloudwaysapps.com',
+    'software_type' => 'product',
+  ),
+  'vendor_panel' => 
+  array (
+    'active' => '0',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+  'user_app' => 
+  array (
+    'active' => '0',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+  'vendor_app' => 
+  array (
+    'active' => '1',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => 'MzY3NzIxNzM=',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+  'deliveryman_app' => 
+  array (
+    'active' => '1',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => 'MzY3NzIxNDg=',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+  'react_web' => 
+  array (
+    'active' => '1',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => 'NDUzNzAzNTE=',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+);
